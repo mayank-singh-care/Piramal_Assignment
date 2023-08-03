b1c298e90fcb7e7d1dc93f3dfde8b9756a87d7af
Mayank Singh <31184015+mayank-singh-care@users.noreply.github.com>
2023-08-03 14:23:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 console.log("hello");
 
-var modal = document.getElementByClassName("popup_main");
+var modal = document.getElementsByClassName("popup_main")[0];
 
 // Get the button that opens the modal
-var btn = document.getElementByClassName("enquire");
+var btn = document.getElementsByClassName("enquire")[0];
 
 // Get the <span> element that closes the modal
-var span = document.getElementByClassName("close")[0];
+var span = document.getElementsByClassName("popup_close")[0];
 
 // When the user clicks the button, open the modal
 btn.onclick = function () {
@@ -21,17 +21,53 @@ span.onclick = function () {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
+  console.log(event.target, modal, event.target == modal);
   if (event.target == modal) {
     modal.style.display = "none";
   }
 };
 
-let form = document.getElementById("contactForm");
-form.addEventListener("submit", (e) => {
+let tourForm = document.getElementById("tourForm");
+tourForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("clicked");
+  document.querySelector(".submit_button").value = "Booking";
+  let data = new FormData(tourForm);
+  fetch(
+    "https://script.google.com/macros/s/AKfycbwnXVOO9pTYnHytyRwWOhs_KJlg5mRNS7onuVZcTwCHG3Jb0gIsw9i6SnA5QeCvqjpc/exec",
+    {
+      method: "POST",
+      body: data,
+    }
+  )
+    .then((res) => res.text())
+    .then((data) => {
+      console.log(data);
+      document.querySelector(".submit_button").value = "Book";
+      alert("Thank you for sharing your details");
+    });
+  data = JSON.stringify(Object.fromEntries(data));
+  console.log(data);
+  fetch(
+    "https://api.propacity.in/api/v1/webhooks/integration/794d3601-ec97-454f-a3b8-6c5961ff8da8/insertLead",
+    {
+      method: "POST",
+      body: data,
+    }
+  )
+    .then((res) => res.text())
+    .then((data) => {
+      console.log(data);
+      alert("Data sent to API successfully");
+    });
+});
+
+let contactForm = document.getElementById("contactForm");
+contactForm.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("clicked");
   document.querySelector(".submit").value = "Submitting";
-  let data = new FormData(form);
+  let data = new FormData(contactForm);
   fetch(
     "https://script.google.com/macros/s/AKfycbwnXVOO9pTYnHytyRwWOhs_KJlg5mRNS7onuVZcTwCHG3Jb0gIsw9i6SnA5QeCvqjpc/exec",
     {
